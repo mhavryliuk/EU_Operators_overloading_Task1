c7adfbe91d9ba5a786e73024e12c1b87062dc7e1
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-04-21 18:01:34 +0300

    Operators overloading - adding the solution.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/* Create a class vector on the plane, class fields - vector coordinates.
+ * In the class, provide different methods for overloading operations.
+ * 
+ * Создать класс вектор на плоскости, поля класса - координаты вектора.
+ * В классе предусмотреть разные методы перегрузки операций. */
+
+namespace _20180313_Vector
+{
+    class Program
+    {
+        static void Main()
+        {
+            Vector vector1 = new Vector(3, 25, 14);
+            Vector vector2 = new Vector(5, 11, 24);
+
+            Console.WriteLine("Demonstration of operator overloading: ==, !=, + and -\n");
+
+            Console.WriteLine($"Coordinates of the first vector: {vector1}");
+            Console.WriteLine($"Coordinates of the second vector: {vector2}");
+            Console.WriteLine();
+
+            if (vector1 == vector2)
+                Console.WriteLine("Vectors are equal");
+            else
+                Console.WriteLine("Vectors are not equal");
+            Console.WriteLine();
+
+            Vector vector3 = vector1 + vector2;
+            Vector vector4 = vector1 - vector2;
+            Vector vector5 = vector1 * vector2;
+
+            Console.WriteLine($"vector1 + vector2: {vector3}");
+            Console.WriteLine($"vector1 - vector2: {vector4}");
+            Console.WriteLine($"vector1 * vector2: {vector5}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+// https://msdn.microsoft.com/ru-ru/library/ms131187(v=vs.110).aspx
+
+namespace _20180313_Vector
+{
+    class Vector : IEquatable<Vector>   // Vector - object type for comparison.
+    {
+        // The coordinates of a point in three-dimensional space
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            try
+            {
+                if (x < 0 && y < 0 && z < 0)
+                {
+                    throw new Exception("This is a test exception that occurs if x, y and z are less than 0\n");
+                }
+                else
+                {
+                    X = x;
+                    Y = y;
+                    Z = z;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Overload the comparison operator ==
+        public static bool operator ==(Vector vector1, Vector vector2)
+        {
+            return Equals(vector1, vector2);
+        }
+
+        // Overload the comparison operator !=
+        public static bool operator !=(Vector vector1, Vector vector2)
+        {
+            return !Equals(vector1, vector2);
+        }
+
+        // Overload the binary operator +
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.X + vector2.X,
+                              vector1.Y + vector2.Y,
+                              vector1.Z + vector2.Z);
+        }
+
+        // Overload the binary operator -
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.X - vector2.X,
+                              vector1.Y - vector2.Y,
+                              vector1.Z - vector2.Z);
+        }
+
+        // Overload the binary operator *
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.X * vector2.X,
+                              vector1.Y * vector2.Y,
+                              vector1.Z * vector2.Z);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}, {2})", X, Y, Z);
+        }
+
+        public bool Equals(Vector other)
+        {
+            if (other is null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return X.Equals(other.X) && Y.Equals(other.Y) && Z.Equals(other.Z);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((Vector)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = X.GetHashCode();
+                hashCode = (hashCode * 397) ^ Y.GetHashCode();
+                hashCode = (hashCode * 397) ^ Z.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
